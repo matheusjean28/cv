29102b9bc6924bc0f7b977ac891182773de7c6ad
matheusjean28 <matheusjean28@gmail.com>
2023-08-26 13:12:06 -0300

    <Feat> (Header, AboutMe) : Add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Components/Header'
 import AboutMe from './Components/AboutMe'
+import Projects from './Components/Projects'
 function App() {
 
   return (
     <>
     <Header/>
     <AboutMe/>
+    <Projects/>
     </>
   )
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './Styles/StylesProjects.css'
+export default function Projects() {
+    return (
+        <>
+            <div className="Projects">
+                <h4>Projetos</h4>
+                <ul>
+                    <li>
+                        <h5>AGILE GEST</h5>
+                        <p>
+                            Plataforma de gestao financeira,
+                            agende, calcule e melhore sua
+                            gestão financeira de forma rapida e segura.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </>
+
+    )
+}
