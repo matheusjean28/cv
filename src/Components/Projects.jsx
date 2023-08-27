fd0c36cc21f3db6fb3555e480e384fe76dc28909
matheusjean28 <matheusjean28@gmail.com>
2023-08-27 13:17:29 -0300

    <Projects.jsx> : (Add media query, adjust color´s to debug and adjust side to objects)

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,11 +4,20 @@ export default function Projects() {
     return (
         <>
             <div className="Projects">
-                <h4>Projetos</h4>
-                <ul>
-                    <li>
+                <h1>Projetos</h1>
+                <ul className="UlProjects">
+                    <li className="LiProjects">
                         <h5>AGILE GEST</h5>
-                        <p>
+                        <p className="PProjects">
+                            Plataforma de gestao financeira,
+                            agende, calcule e melhore sua
+                            gestão financeira de forma rapida e segura.
+                        </p>
+                    </li>
+
+                    <li className="LiProjectsRight">
+                        <h5>AGILE GEST</h5>
+                        <p className="PProjects">
                             Plataforma de gestao financeira,
                             agende, calcule e melhore sua
                             gestão financeira de forma rapida e segura.
